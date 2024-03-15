d01ca795028e64c3a735e14e53105b3a45982dac
raysas <rayane.s.adam@gmail.com>
2024-03-15 11:34:51 +0200

    added func

--- src/network_calculations.py
+++ src/network_calculations.py
@@ -7,38 +7,7 @@ import sys
 import pandas as pd
 import networkx as nx
 import numpy as np
-
-def _log_odds(df: pd.DataFrame) -> float:
-    '''
-    This helper function takes a 2x2 contingency table and returns the log odds ratio.
-    The contingency matrix is of this form: #make for gene absence/precense and resistant/susceptible
-    |           | gene absent | gene present |
-    |-----------|-------------|--------------|
-    |resistant  |    a        |     b        |
-    |susceptible|    c        |     d        |
-
-    the log odds ratio is calculated as:
-        $log(a*d/b*c)$
-        which is the log of the odds of the gene being present in resistant samples over the gene being present in susceptible samples
-    
-    param:
-        - df: (pd.DataFrame) 2x2 contingency table
-
-    output:
-        - log_odds: (float) log odds ratio
-    '''
-    a = df.iloc[0,0]
-    b = df.iloc[0,1]
-    c = df.iloc[1,0]
-    d = df.iloc[1,1]
-    # if any of the values is 0, add 0.5 to all values to avoid division by 0 or getting a log of 0
-    if a*d == 0 or b*c == 0:
-        a+=0.5; b+=0.5; c+=0.5; d+=0.5
-    log_odds = np.log((a*d)/(b*c))
-    return log_odds
-
-_get_pheno_df= lambda pheno_csv_path: pd.read_csv(pheno_csv_path, index_col=0).astype(int)
-_get_samples_list= lambda species_ids_path: open(species_ids_path).readlines()
+from data_utils import *
 
 
 def _label_samples(pheno_df: pd.DataFrame, samples_list:list) :
@@ -66,3 +35,32 @@ def _label_samples(pheno_df: pd.DataFrame, samples_list:list) :
 
 def _count_presence_matrix(df: pd.DataFrame) -> int:
     return
+
+def _log_odds(df: pd.DataFrame) -> float:
+    '''
+    This helper function takes a 2x2 contingency table and returns the log odds ratio.
+    The contingency matrix is smtg of this form: for gene absence/precense and resistant/susceptible
+    |           | gene absent | gene present |
+    |-----------|-------------|--------------|
+    |resistant  |    a        |     b        |
+    |susceptible|    c        |     d        |
+
+    the log odds ratio is calculated as:
+        log(a*d/b*c)
+        which is the log of the odds of the gene being present in resistant samples over the gene being present in susceptible samples
+    
+    param:
+        - df: (pd.DataFrame) 2x2 contingency table
+
+    output:
+        - log_odds: (float) log odds ratio
+    '''
+    a = df.iloc[0,0]
+    b = df.iloc[0,1]
+    c = df.iloc[1,0]
+    d = df.iloc[1,1]
+    # if any of the values is 0, add 0.5 to all values to avoid division by 0 or getting a log of 0
+    if a*d == 0 or b*c == 0:
+        a+=0.5; b+=0.5; c+=0.5; d+=0.5
+    log_odds = np.log((a*d)/(b*c))
+    return log_odds
