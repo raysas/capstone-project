3c32bbb251470599f20cd60f78e6503ab12f5d40
raysas <rayane.s.adam@gmail.com>
2024-05-11 14:58:35 +0300

    -- finalized analysis

--- src/analysis/useful_cmds.sh
+++ src/analysis/useful_cmds.sh
@@ -16,9 +16,9 @@ for antibiotic in streptomycin sulfamethoxazole cefalothin tetracycline trimetho
     mkdir ../../results/${species}_${antibiotic}
 done
 
-# -- cpying the tetracycline analysis notebook and replacing the antibiotic name
-for antibiotic in streptomycin sulfamethoxazole cefalothin trimethoprim amoxicillin ampicillin levofloxacin ciprofloxacin; do
-    cp Escherichia_coli_tetracycline_analysis.ipynb Escherichia_coli_${antibiotic}_analysis.ipynb
+# -- cpying the amoxicillin analysis notebook and replacing the antibiotic name
+for antibiotic in streptomycin sulfamethoxazole cefalothin trimethoprim tetracycline ampicillin levofloxacin ciprofloxacin; do
+    cp Escherichia_coli_amoxicillin_analysis.ipynb Escherichia_coli_${antibiotic}_analysis.ipynb
     sed -i "s/tetracycline/${antibiotic}/g" Escherichia_coli_${antibiotic}_analysis.ipynb
 done
 
